42ba16136f4e22b272002c89fa3db58b1c17fa74
aruyamam <aruyamam@gmail.com>
2019-03-23 14:09:35 +0900

    Using Express.js middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,26 @@
 import path from 'path';
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression';
 
 const root = path.join(__dirname, '../../');
 const app = express();
+
+app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", 'data:', '*.amazonaws.com'],
+    },
+  }),
+);
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+app.use(compress());
+app.use(cors());
 
 app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'uploads')));
