a7d73c87b118d3f7654236af33daa0fa11126da1
aruyamam <aruyamam@gmail.com>
2019-03-26 14:32:35 +0900

    Combining Express.js with Apollo

--- src/server/index.js
+++ src/server/index.js
@@ -3,27 +3,42 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import compress from 'compression';
+import services from './services';
 
 const root = path.join(__dirname, '../../');
 const app = express();
 
-app.use(helmet());
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-inline'"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
-      imgSrc: ["'self'", 'data:', '*.amazonaws.com'],
-    },
-  }),
-);
+if (process.env.NODE_ENV === 'production') {
+  app.use(helmet());
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", 'data:', '*.amazonaws.com'],
+      },
+    }),
+  );
+  app.use(compress());
+  app.use(cors());
+}
+
 app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
-app.use(compress());
-app.use(cors());
 
 app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'uploads')));
+
+const serviceNames = Object.keys(services);
+// console.log(services);
+for (let i = 0; i < serviceNames.length; i += 1) {
+  const name = serviceNames[i];
+  if (name === 'graphql') {
+    services[name].applyMiddleware({ app });
+  } else {
+    app.use(`/${name}`, services[name]);
+  }
+}
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(root, '/dist/client/index.html'));
---
+++ src/server/services/graphql/resolver.js
@@ -0,0 +1,40 @@
+const posts = [
+  {
+    id: 2,
+    text: 'Lorem ipsum',
+    user: {
+      avatar: '/uploads/avatar1.png',
+      username: 'Test User',
+    },
+  },
+  {
+    id: 1,
+    text: 'Lorem ipsum',
+    user: {
+      avatar: '/uploads/avatar2.png',
+      username: 'Test User 2',
+    },
+  },
+];
+
+const resolver = {
+  RootQuery: {
+    posts(root, args, context) {
+      return posts;
+    },
+  },
+  RootMutation: {
+    addPost(root, { post, user }, context) {
+      const postObject = {
+        ...post,
+        user,
+        id: posts.length + 1,
+      };
+      posts.push(postObject);
+
+      return postObject;
+    },
+  },
+};
+
+export default resolver;
