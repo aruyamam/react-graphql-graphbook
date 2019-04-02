71ceac86bf69afd76f24fadfbc0721511d0112af
aruyamam <aruyamam@gmail.com>
2019-04-02 15:06:12 +0900

    Using Sequelize with Apollo

--- src/server/services/graphql/resolver.js
+++ src/server/services/graphql/resolver.js
@@ -1,43 +1,29 @@
 import logger from '../../helpers/logger';
 
-const posts = [
-  {
-    id: 2,
-    text: 'Lorem ipsum',
-    user: {
-      avatar: '/uploads/avatar1.png',
-      username: 'Test User',
-    },
-  },
-  {
-    id: 1,
-    text: 'Lorem ipsum',
-    user: {
-      avatar: '/uploads/avatar2.png',
-      username: 'Test User 2',
-    },
-  },
-];
+export default function resolver() {
+  const { db } = this;
+  const { Post } = db.models;
 
-const resolver = {
-  RootQuery: {
-    posts(root, args, context) {
-      return posts;
+  const resolver = {
+    RootQuery: {
+      posts(root, args, context) {
+        return Post.findAll({ order: [['createdAt', 'DESC']] });
+      },
     },
-  },
-  RootMutation: {
-    addPost(root, { post, user }, context) {
-      const postObject = {
-        ...post,
-        user,
-        id: posts.length + 1,
-      };
-      posts.push(postObject);
-      logger.log({ level: 'info', message: 'Post was created' });
+    RootMutation: {
+      addPost(root, { post, user }, context) {
+        const postObject = {
+          ...post,
+          user,
+          id: posts.length + 1,
+        };
+        posts.push(postObject);
+        logger.log({ level: 'info', message: 'Post was created' });
 
-      return postObject;
+        return postObject;
+      },
     },
-  },
-};
+  };
 
-export default resolver;
+  return resolver;
+}
