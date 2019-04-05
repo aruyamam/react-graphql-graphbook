778d62212c60232d184e73aa8d782f3ac6fce87f
aruyamam <aruyamam@gmail.com>
2019-04-05 14:53:28 +0900

    One-to-one relationships in Sequelize

--- src/server/models/post.js
+++ src/server/models/post.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
   );
 
   Post.associate = function (models) {
-    // associations can be defined here
+    Post.belongsTo(models.User);
   };
 
   return Post;
